Fix whole bundle area calculation

analyze.py
@@ -121,7 +121,7 @@ def analyze_image(filepath):
     areas = calculate_areas(contrast_channels)
 
     # Calculate cross-sectional area for whole bundle
-    contour_points = draw_bundle_contour(channels)
+    contour_points, original_size = draw_bundle_contour(channels)
     hull = cv2.convexHull(contour_points)
     whole_bundle_mask = np.zeros_like(list(channels.values())[0])
     cv2.drawContours(
@@ -131,7 +131,7 @@ def analyze_image(filepath):
         (255,),
         -1,
     )
-    whole_bundle_area = cv2.countNonZero(whole_bundle_mask) * 0.153**2
+    whole_bundle_area = cv2.countNonZero(whole_bundle_mask) * 0.153**2 * original_size[0] / 512 * original_size[1] / 512
     areas["whole"] = whole_bundle_area
 
     # Count particles in each layer

user_menu.py
@@ -284,6 +284,7 @@ def draw_bundle_contour(channels):
     whole_bundle = np.zeros_like(channels_list[0])
     for c in channels_list:
         whole_bundle = cv2.add(whole_bundle, c)
+    whole_bundle_size = whole_bundle.shape
     whole_bundle = cv2.resize(whole_bundle, PREVIEW_SIZE)
 
     points = []
@@ -325,7 +326,7 @@ def draw_bundle_contour(channels):
         elif key == ord("s"):
             cv2.destroyAllWindows()
             pts = np.array([points + [points[0]]]).reshape((-1, 1, 2))
-            return pts 
+            return pts, whole_bundle_size
 
 def set_contrast(image, composite=False):
     """